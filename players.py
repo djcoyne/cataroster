9c3fd9d16f8607c23440fd13ac163812146dd69b
David Coyne <djcoyne@gmail.com>
2024-04-30 14:30:35 -0400

    Update players.py

--- players.py
+++ players.py
@@ -3,13 +3,37 @@ This file defines a player class and the attributes/functions available for that
 """
 import classes
 
-class Character(Player):
-  
+class Character:
+  def __init__(self, n, c, s, **kwargs):
+    name = n
+    class = c
+    spec = s
+
+    if c=='Death Knight':
+      if s=='Blood':
+        if 'role' in kwargs:
+          r = role
+          DKBlood(
+      
+    
+
+  def buff_check(self):
+    print(name
+    
+    
 
 class Player:
-  def __init__(self, name, character, **kwargs):
+  def __init__(self, name, **kwargs):
     self.name = name
-    self.character
+    self.charlist = []
+
+  def add_character(self, name, class, spec, **kwargs):
+    if 'role' in kwargs:
+      r = role
+      Character(name, class, spec, role=r)
+    else:
+      Character(name, class, spec)
+    self.charlist.append('name')
       
 
     
