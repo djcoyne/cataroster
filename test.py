b8a54435960a661c0cfbbf7634a744228942974e
David Coyne <djcoyne@gmail.com>
2024-05-11 15:31:33 -0400

    adding in functionality

--- test.py
+++ test.py
@@ -28,13 +28,33 @@ for index in pl.index:
         else:
             players[index].add_character(name=pl['name'][index],cl=pl['cl'][index],spec=pl['spec'][index],role=pl['role'][index])
 
-
 # Create the list of Mains
 r = []
 for x in players:
     r.append(x.charlist[0])
 
+# Reset Button function
+
+def resetPlayers():
+    for x in raider:
+        x.reset()
+    rlist=[]
+    calculateBuffs(rlist)
+
+# Calculate Buffs function
+
+def calculateBuffs(rlist):
+    raidbuffs = pd.read_csv('raidbuffs.csv', names=['Buff'])
+    availbuffs = []
+    for x in rlist:
+        availbuffs.extend(x.a.buffs)
+
+    availbuffs = set(availbuffs)
+    missbuffs = pd.concat([raidbuffs,pd.DataFrame(availbuffs, columns=['Buff'])]).drop_duplicates(keep=False)
+    return missbuffs
+
 #Define Main Window
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -126,111 +146,6 @@ app = QApplication(sys.argv)
 
 ui = MainWindow()
 ui.show()
-
-"""
-OLD UI SECTION
-
-groups=[]
-i=1
-while i < 6:
-    groups.append(tk.Label(ui,
-                           text = "Group " + str(i)
-                           ))
-    groups[-1].grid(row=6, column = i, sticky='s')
-    i+=1
-
-Raid positions
-
-pos=[]
-i=1
-while i < 6:
-    j=8
-    while j < 13:
-      pos.append(tk.Frame(ui,
-                          bg='gray'
-                          ).grid(row=j, column=i,sticky='nsew', padx=3, pady=3 ))
-      j+=1
-    i+=1
-      
-
-Draggables
-
-rlist = []
-raider=[]
-i=0    
-j=0
-j=0
-
-for x in r:
-    raider.append(dm.dragManager(ui,
-                        raider = x,
-    raider.append(dm.dragManager(ui,
-                        raider = x,
-                        text=x.n + "; "+ x.s+" ("+x.a.r+")",
-                        color=x.a.c,
-                        color=x.a.c,
-                        height=2,
-                        width=30,
-                        x=i,
-                        y=j))
-    raider[-1].grid(row=i, column=j, sticky='nsew', padx=3, pady=3)
-    i+=1
-    if i % 5==0:
-        i=0
-        j+=1
-
-
-Reset Button
-
-
-def resetPlayers():
-    for x in raider:
-        x.reset()
-    rlist=[]
-    calculateBuffs(rlist)
-
-reset_butt = tk.Button(text = "Reset",
-    width=7,
-    height=2,
-    bg='red',
-    fg='black',
-    command = resetPlayers
-    )
-
-reset_butt.grid(row=1,column=8, sticky='new')
-
-
-Calculate Buffs
-
-def calculateBuffs(rlist):
-    raidbuffs = pd.read_csv('raidbuffs.csv', names=['Buff'])
-    availbuffs = []
-    for x in rlist:
-        availbuffs.extend(x.a.buffs)
-
-    availbuffs = set(availbuffs)
-    missbuffs = pd.concat([raidbuffs,pd.DataFrame(availbuffs, columns=['Buff'])]).drop_duplicates(keep=False)
-    return missbuffs.to_string(index=False, header=False)
-
-calc_butt = tk.Button(text = "Calculate Buffs",
-                  width=7,
-                  height=2,
-                  bg='green',
-                  fg='black',
-                  command = calculateBuffs(rlist)
-                  )
-calc_butt.grid(row=0, column=8, sticky='sew')
-
-
-Results Section
-
-m1 = tk.Label(text="Your raid is missing:\n"+calculateBuffs(rlist),
-              font= ('Arial'))
-m1.grid(row=6, column=8, rowspan=7, sticky='e')
-
-ui.mainloop()
-"""
-
 app.exec()
 
     
